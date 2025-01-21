495b6bc97f57aa36b16841854a065fdfe545540d
Nicolás Filippi Farmar <nico.filippi@hotmail.com>
2025-01-21 19:45:51 -0300

    Added default configs

---
+++ src/amzcube_commons.h
@@ -0,0 +1,11 @@
+#ifndef AMZCUBE_COMMONS_H_
+#define AMZCUBE_COMMONS_H_
+
+#define DEBUG_INFO true
+#define DEBUG_WARNING true
+#define DEBUG_ERROR true
+
+#include <Arduino.h>
+#include "serial/serial_commons.h"
+
+#endif // AMZCUBE_COMMONS_H_
