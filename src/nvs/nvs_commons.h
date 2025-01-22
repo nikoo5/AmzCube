f3f0cd4ce0ef504570e7a018af9e09a63c8d703b
Nicolás Filippi Farmar <nico.filippi@hotmail.com>
2025-01-22 05:27:43 -0300

    Add WiFi credentials and initialization GIF constants to NVS commons

--- src/nvs/nvs_commons.h
+++ src/nvs/nvs_commons.h
@@ -1,6 +1,14 @@
 #ifndef NVS_COMMONS_H_
 #define NVS_COMMONS_H_
 
+namespace NVS
+{
+  const char *WIFI_SSID = "ssid_wifi";
+  const char *WIFI_PASSWORD = "wifi_password";
+
+  const char *INIT_GIF = "init_gif";
+}
+
 #include <nvs.h>
 
 #include "amzcube_commons.h"
