ba56d97d0fa71fb50e34dc299e3dfdcbae943a8b
Nicolás Filippi Farmar <nico.filippi@hotmail.com>
2025-01-21 22:18:22 -0300

    Refactor NVS and SPIFFS function names for consistency; add GIF handling in main logic

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,29 @@ void setup()
   SerialSetup();
 
   // uncomment to set Wifi SSID and password for the first time
-  // setNvsValue("wifi_ssid", "<your_wifi_ssid>");
-  // setNvsValue("wifi_password", "<your_wifi_pass>");
+  // NVSSetValue("wifi_ssid", "<your_wifi_ssid>");
+  // NVSSetValue("wifi_password", "<your_wifi_pass>");
 
-  SpiffsSetup();
+  SPIFFSSetup();
   WiFiSetup();
   TFTSetup();
-
-  TFTFillScreen(TFT_GREEN);
+  GifSetup();
 }
 
 void loop()
 {
-  if (!SpiffsFileExists("/images/gifs/eyes.gif"))
+  if (!SPIFFSFileExists("/images/gifs/eyes.gif"))
   {
-    char *response = executeGetRequest("https://raw.githubusercontent.com/nikoo5/AmzCube/refs/heads/master/web/images/gifs/eyes.gif", "/certs/DigiCert_CA1.crt");
+    char *response = WiFiExecuteGetRequest("https://raw.githubusercontent.com/nikoo5/AmzCube/refs/heads/master/web/images/gifs/eyes.gif", "/certs/DigiCert_CA1.crt");
 
     if (response != nullptr)
     {
-      SpiffsWriteFileContent("/images/gifs/eyes.gif", response);
+      SPIFFSWriteFileContent("/images/gifs/eyes.gif", response);
     }
   }
   else
   {
+    if (!TFTDrawAnimatedGif("/images/gifs/eyes.gif", 0, 0))
+      delay(5000);
   }
 }
--- src/main.h
+++ src/main.h
@@ -5,5 +5,6 @@
 #include "serial/serial_commons.h"
 #include "wifi/wifi_commons.h"
 #include "tft/tft_commons.h"
+#include "gif/gif_commons.h"
 
 #endif // MAIN_H_
